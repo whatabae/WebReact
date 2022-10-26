f1bfc452599858d092ecef939b5ed42252b4c1b7
Lufinas <kim980728@gmail.com>
2022-10-26 21:20:25 +0900

    2022-10-26 commit

--- src/chapter_12/Prj_9_1_2/index.js
+++ src/chapter_12/Prj_9_1_2/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import WelcomeDialog from './chapter_12/Prj_9_1_2/Specialization.jsx';
+import PassingCount from './chapter_12/PassingCount';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <WelcomeDialog/>
+    <PassingCount/>
 );
