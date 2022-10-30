dc1bd4e56fa5daa53acb64d58841122f81cb3059
whatabae <kim980728@gmail.com>
2022-10-30 21:21:23 +0900

    1024 index commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import ProfileCard from './chapter_12/ProfileCard';
+import PassingCount from './chapter_12/PassingCount';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <ProfileCard/>
+    <PassingCount/>
 );
